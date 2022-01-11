c53150953519c80664c351e05c8eeee0ad613473
Hiko <amir.amuri@yahoo.de>
2022-01-11 11:44:41 +0100

    First init

---
+++ WineQuality.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/User/Desktop/Uni/1.Vorlesungen/3.Semester_WS_2021-2022/02.Projekte/03.Assistenzsysteme/Projektarbeit_Wein_Qualität")
+
+WeinR <- read.csv("winequality-red.csv", header=TRUE, sep=";",fill=TRUE)
+WeinW <- read.csv("winequality-white.csv", header=TRUE, sep=";",fill=TRUE)
+
